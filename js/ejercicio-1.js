2b67bcc6b2fef716d72c875abcacce284f0abebe
juanmllanos <juan_m@hotmail.com.ar>
2023-03-08 21:29:22 -0300

    Correccion gramatica linea 7

--- js/ejercicio-1.js
+++ js/ejercicio-1.js
@@ -4,7 +4,7 @@ if (edadUsuario >= 18 && edadUsuario <= 110) {
 } else if (edadUsuario > 0 && edadUsuario < 18){
 	document.write('Sos menor de edad, no podes manejar.');
 } else if (edadUsuario < 0 || edadUsuario > 110) {
-	document.write('Ingrese un numero invalido.');
+	document.write('Ingresaste un numero invalido.');
 } else {
 	document.write('No ingresaste un numero.');
 };
