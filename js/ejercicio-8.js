293778ba7698aa5df77bd3d5b298557f467f4041
juanmllanos <juan_m@hotmail.com.ar>
2023-03-12 17:02:14 -0300

    ejercicio 8 terminado

---
+++ js/ejercicio-8.js
@@ -0,0 +1,21 @@
+let pisosPiramide = parseInt(prompt('Ingrese el numero de escalones que quiera que tenga la piramide (Entre 1 y 50).'));
+
+while (pisosPiramide < 1 || pisosPiramide > 50 || !pisosPiramide) {
+	if (pisosPiramide < 1 || pisosPiramide > 50) {
+		alert('Numero Invalido.');
+		pisosPiramide = parseInt(prompt('Escriba el numero de escalones que quiera que tenga la piramide. (Entre 1 y 50)'));
+	};
+	if (!pisosPiramide) {
+		alert('No ingresaste un Numero.');
+		pisosPiramide = parseInt(prompt('Escriba el numero de escalones que quiera que tenga la piramide. (Entre 1 y 50)'));
+	};
+};
+
+for (let escalones = 1; escalones <= pisosPiramide; escalones++) {
+	for (let numero = 1; numero <= escalones; numero++) {
+		document.write(`${escalones}`);
+		if (numero === escalones) {
+			document.write(`<br>`);
+		};
+	};
+};
